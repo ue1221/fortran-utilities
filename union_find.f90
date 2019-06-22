659eaa33827fb38612eae3777df2538713c03b20
ue1221 <49748628+ue1221@users.noreply.github.com>
2019-06-22 23:55:18 +0900

    Defined union_find as type.

--- union_find.f90
+++ union_find.f90
@@ -1,70 +1,77 @@
 module mod_union_find
   implicit none
-  integer :: n
-  integer, allocatable :: par(:), rnk(:), siz(:)
+  type union_find
+    integer :: n
+    integer, pointer :: par(:), rnk(:), siz(:)
+  end type union_find
   private
-  public :: initialize, finalize, find, same, unite, size_of
+  public :: init_union_find, release_union_find, find, same, unite, size_of
 contains
-  subroutine initialize(m)
+  subroutine init_union_find(uf,n)
     implicit none
-    integer, intent(in) :: m
+    type(union_find), intent(inout) :: uf
+    integer, intent(in) :: n
     integer :: i
-    n = m
-    if (allocated(par)) deallocate(par)
-    if (allocated(rnk)) deallocate(rnk)
-    if (allocated(siz)) deallocate(siz)
-    allocate(par(n),rnk(n),siz(n))
-    rnk = 0
-    siz = 1
+    if (associated(uf%par)) deallocate(uf%par)
+    if (associated(uf%rnk)) deallocate(uf%rnk)
+    if (associated(uf%siz)) deallocate(uf%siz)
+    allocate(uf%par(n),uf%rnk(n),uf%siz(n))
+    uf%rnk = 0
+    uf%siz = 1
     do i = 1, n
-      par(i) = i
+      uf%par(i) = i
     end do
     return
-  end subroutine initialize
-  subroutine finalize()
+  end subroutine init_union_find
+  subroutine release_union_find(uf)
     implicit none
-    if (allocated(par)) deallocate(par)
-    if (allocated(rnk)) deallocate(rnk)
-    if (allocated(siz)) deallocate(siz)
+    type(union_find), intent(inout) :: uf
+    if (associated(uf%par)) deallocate(uf%par)
+    if (associated(uf%rnk)) deallocate(uf%rnk)
+    if (associated(uf%siz)) deallocate(uf%siz)
     return
-  end subroutine finalize
-  recursive function find(i) result(j)
+  end subroutine release_union_find
+  recursive function find(uf,i) result(j)
     implicit none
+    type(union_find), intent(inout) :: uf
     integer, intent(in) :: i
     integer :: j
-    if (par(i).ne.i) then
-      par(i) = find(par(i))
+    if (uf%par(i).ne.i) then
+      uf%par(i) = find(uf,uf%par(i))
     end if
-    j = par(i)
+    j = uf%par(i)
     return
   end function find
-  function same(i,j) result(y)
+  function same(uf,i,j) result(y)
     implicit none
+    type(union_find), intent(inout) :: uf
     integer, intent(in) :: i, j
     logical :: y
-    y = find(i).eq.find(j)
+    y = find(uf,i).eq.find(uf,j)
   end function same
-  subroutine unite(i,j)
+  subroutine unite(uf,i,j)
     implicit none
+    type(union_find), intent(inout) :: uf
     integer, intent(in) :: i, j
     integer :: x, y
-    x = find(i)
-    y = find(j)
+    x = find(uf,i)
+    y = find(uf,j)
     if (x.eq.y) return
-    if (rnk(x).lt.rnk(y)) then
-      par(x) = y
-      siz(y) = siz(y)+siz(x)
+    if (uf%rnk(x).lt.uf%rnk(y)) then
+      uf%par(x) = y
+      uf%siz(y) = uf%siz(y)+uf%siz(x)
     else
-      par(y) = x
-      siz(x) = siz(x)+siz(y)
-      if (rnk(x).eq.rnk(y)) rnk(x) = rnk(x)+1
+      uf%par(y) = x
+      uf%siz(x) = uf%siz(x)+uf%siz(y)
+      if (uf%rnk(x).eq.uf%rnk(y)) uf%rnk(x) = uf%rnk(x)+1
     end if
     return
   end subroutine unite
-  function size_of(i) result(s)
+  function size_of(uf,i) result(s)
     implicit none
+    type(union_find), intent(inout) :: uf
     integer, intent(in) :: i
     integer :: s
-    s = siz(find(i))
+    s = uf%siz(find(uf,i))
   end function size_of
 end module mod_union_find
